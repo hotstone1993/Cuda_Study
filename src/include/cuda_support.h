6ce84d91d698ea7f5f5606e56e276f5a3944c943
박신석 <hotstone1993@gmail.com>
2023-02-03 03:18:01 +0900

    add helper header

--- src/include/cuda_support.h
+++ src/include/cuda_support.h
@@ -13,6 +13,24 @@ inline void initCUDA() {
     }
 }
 
+
 #define CUDA_MALLOC(ptr, size, TYPE) {\
-    cudaMalloc((void**)&ptr, size * sizeof(TYPE)); \
+    cudaError_t cudaStatus = cudaMalloc((void**)&ptr, size * sizeof(TYPE)); \
+    if (cudaStatus != cudaSuccess) { \
+        throw "cudaMalloc failed!"; \
+    }\
+}
+
+#define CUDA_ALLOC(INPUT_SIZE, INPUT_TYPE, OUTPUT_SIZE, OUTPUT_TYPE) { \
+    host_input = new INPUT_TYPE[INPUT_SIZE]; \
+    host_output = new OUTPUT_TYPE[OUTPUT_SIZE]; \
+    CUDA_MALLOC(device_input, INPUT_SIZE, INPUT_TYPE); \
+    CUDA_MALLOC(device_output, OUTPUT_SIZE, OUTPUT_TYPE); \
+}
+
+#define CUDA_DEALLOC() { \
+    delete[] host_input; \
+    delete[] host_output; \
+    cudaFree(device_input); \
+    cudaFree(device_output); \
 }
---
+++ src/include/utils.h
@@ -0,0 +1,16 @@
+#include <cmath>
+#include <limits>
+#include <iostream>
+#include <type_traits>
+#include <algorithm>
+
+
+// https://en.cppreference.com/w/cpp/types/numeric_limits/epsilon
+template<class T>
+typename std::enable_if<!std::numeric_limits<T>::is_integer, bool>::type almost_equal(T x, T y, int ulp) {
+    // the machine epsilon has to be scaled to the magnitude of the values used
+    // and multiplied by the desired precision in ULPs (units in the last place)
+    return std::fabs(x-y) <= std::numeric_limits<T>::epsilon() * std::fabs(x+y) * ulp
+        // unless the result is subnormal
+        || std::fabs(x-y) < std::numeric_limits<T>::min();
+}
