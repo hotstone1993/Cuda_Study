e9e9678ff926d8d0153fd3b3df63685a3e00378c
박신석 <hotstone1993@gmail.com>
2023-02-12 20:43:07 +0900

    load image

--- src/1.image/1_0_Blur_MT.cpp
+++ src/1.image/1_0_Blur_MT.cpp
@@ -4,7 +4,7 @@ constexpr size_t THREAD_COUNT = 8;
 
 template <class T1, class T2>
 void image::blur::run_mt(std::vector<T1*>& inputs, std::vector<T2*>& outputs) {
-    ThreadPool tp(THREAD_COUNT); 
+    ThreadPool tp(THREAD_COUNT);
 }
 
 template void image::blur::run_mt(std::vector<TARGET_TYPE*>& inputs, std::vector<TARGET_TYPE*>& outputs);
--- src/1.image/1_0_Blur_Support.h
+++ src/1.image/1_0_Blur_Support.h
@@ -4,12 +4,23 @@
 #include "1_0_Blur_MT.h"
 #include "1_0_Blur.cuh"
 
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
 namespace image::blur {
+    uint8_t *data = nullptr;
+
     template <class T1, class T2>
-    void setup(std::vector<T1*>& inputs, std::vector<T2*>& outputs) {}
+    void setup(std::vector<T1*>& inputs, std::vector<T2*>& outputs) {
+        char const* fileName = "../data/1.image/test_image.jpg";
+        int width, height, pixel;
+        data = stbi_load(fileName, &width, &height, &pixel, 0); // red, green, blue
+    }
         
     template <class T1, class T2>
-    void destroy(std::vector<T1*>& inputs, std::vector<T2*>& outputs) {}
+    void destroy(std::vector<T1*>& inputs, std::vector<T2*>& outputs) {
+        stbi_image_free(data);
+    }
 }
 
 #endif IMAGE_BLUR_SUPPORT
