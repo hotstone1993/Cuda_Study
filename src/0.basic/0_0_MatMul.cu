b498508d22c559c2befed6eaa556599955ea1cf5
박신석 <hotstone1993@gmail.com>
2023-02-12 21:53:00 +0900

    seperate target type

--- src/0.basic/0_0_MatMul.cu
+++ src/0.basic/0_0_MatMul.cu
@@ -1,6 +1,6 @@
 #include "0_0_MatMul.cuh"
 
-__global__ void mulMatrix(TARGET_TYPE* c, const TARGET_TYPE* a, const TARGET_TYPE* b, const unsigned int N)
+__global__ void mulMatrix(TARGET_OUTPUT_TYPE* c, const TARGET_INPUT_TYPE* a, const TARGET_INPUT_TYPE* b, const unsigned int N)
 {
     int col = blockDim.x * blockIdx.x + threadIdx.x;
     int row = blockDim.y * blockIdx.y + threadIdx.y;
@@ -8,7 +8,7 @@ __global__ void mulMatrix(TARGET_TYPE* c, const TARGET_TYPE* a, const TARGET_TYP
     if (row >= N || col >= N)
         return;
     
-    TARGET_TYPE sum = 0;
+    TARGET_OUTPUT_TYPE sum = 0;
 
     for (unsigned int idx = 0; idx < N; ++idx) {
         sum += (a[N * row + idx] * b[idx * N + col]);
@@ -17,10 +17,10 @@ __global__ void mulMatrix(TARGET_TYPE* c, const TARGET_TYPE* a, const TARGET_TYP
 }
 
 
-__global__ void mulMatrixWithSharedMemory(TARGET_TYPE* c, const TARGET_TYPE* a, const TARGET_TYPE* b, const unsigned int N)
+__global__ void mulMatrixWithSharedMemory(TARGET_OUTPUT_TYPE* c, const TARGET_INPUT_TYPE* a, const TARGET_INPUT_TYPE* b, const unsigned int N)
 {
-    __shared__ TARGET_TYPE tempA[THREADS][THREADS];
-    __shared__ TARGET_TYPE tempB[THREADS][THREADS];
+    __shared__ TARGET_INPUT_TYPE tempA[THREADS][THREADS];
+    __shared__ TARGET_INPUT_TYPE tempB[THREADS][THREADS];
 
     int col = blockDim.x * blockIdx.x + threadIdx.x;
     int row = blockDim.y * blockIdx.y + threadIdx.y;
@@ -28,7 +28,7 @@ __global__ void mulMatrixWithSharedMemory(TARGET_TYPE* c, const TARGET_TYPE* a, 
     int localCol = threadIdx.x;
     int localRow = threadIdx.y;
 
-    TARGET_TYPE sum = 0;
+    TARGET_OUTPUT_TYPE sum = 0;
 
     for (unsigned int bid = 0; bid < ceil((float)N / blockDim.x); ++bid) {
         if (row < N && bid * blockDim.x + localCol < N) {
@@ -82,4 +82,4 @@ void basic::matmul::run(std::vector<T1*>& inputs, std::vector<T2*>& outputs) {
     }
 }
 
-template void basic::matmul::run(std::vector<TARGET_TYPE*>& inputs, std::vector<TARGET_TYPE*>& outputs);
+template void basic::matmul::run(std::vector<TARGET_INPUT_TYPE*>& inputs, std::vector<TARGET_OUTPUT_TYPE*>& outputs);
